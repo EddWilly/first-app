ada03a5193f9120db2929adf5371b73b9c497692
EddWilly <lawiletxd@live.com>
2023-04-20 16:25:16 -0300

    first changes

---
+++ src/pages/indexComponent.tsx
@@ -0,0 +1,14 @@
+import { PageProps } from "gatsby";
+import React from "react";
+
+const IndexComponent: React.FC<PageProps> = () => {
+    return (
+        <div></div>
+    )
+}
+
+export function IndexComponent2() {
+    return (
+        <div></div>
+    )
+}
